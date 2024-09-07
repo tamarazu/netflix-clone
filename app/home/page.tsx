85f00f5fc880f9f0ab67e1c28b26caf8d0908704
tamarazu <tamarazulaika.u@gmail.com>
2024-09-07 14:47:08 +0700

    remove dry code

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -7,17 +7,24 @@ import Recommendation from "@/components/recommendation";
 type Props = {};
 
 export default function Page({}: Props) {
+  const recommendationTitles = [
+    "Trending Now",
+    "Popular on Netflix",
+    "Top 10 in Indonesia",
+    "My List",
+    "Korean Dramas",
+    "Indonesian Movies"
+  ]
   return (
     <div className="bg-black text-white relative min-h-screen w-full">
       <Banner/>
       <Previews/>
       <ContiueWatch/>
-      <Recommendation name="Popular on Netflix"/>
-      <Recommendation name="Trending Now"/>
-      <Recommendation name="Top 10 in Indonesia"/>
-      <Recommendation name="My List>"/>
-      <Recommendation name="Korean Dramas"/>
-      <Recommendation name="Indonesian Movies"/>
+      {
+        recommendationTitles.map((name, index) => (
+          <Recommendation name={name}/>
+        ))
+      }
     </div>
   );
 }
