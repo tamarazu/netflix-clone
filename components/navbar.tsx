36eb3e58905057db74bacc15e55a1bf2e3cf81ea
tamarazu <tamarazulaika.u@gmail.com>
2024-09-13 22:16:20 +0700

    add navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -6,23 +6,18 @@ import {
   ArrowDownTrayIcon,
   Bars3Icon,
 } from "@heroicons/react/24/outline";
-import { usePathname, useSearchParams } from "next/navigation";
 
 type Props = {};
 
 export const Navbar = (props: Props) => {
-  // const pathname = usePathname()
-  // const searchParams = useSearchParams()
-  // useEffect(() => {
-  //   // Do something here...
-  // }, [pathname, searchParams])
+
   return (
-    <div className="w-full flex justify-around fixed bg-stone-900 text-stone-400 bottom-0 bg-white p-4 ">
-      <HomeIcon height={40} width={40} className={`text-white`} />
-      <MagnifyingGlassIcon height={40} width={40} />
-      <RectangleStackIcon height={40} width={40} />
-      <ArrowDownTrayIcon height={40} width={40} />
-      <Bars3Icon height={40} width={40} />
+    <div className="w-full flex justify-around fixed bg-stone-900 text-stone-400 bottom-0 bg-stone-900 p-4 ">
+      <HomeIcon height={28} width={28} color="white" />
+      <MagnifyingGlassIcon height={28} width={28} />
+      <RectangleStackIcon height={28} width={28} />
+      <ArrowDownTrayIcon height={28} width={28} />
+      <Bars3Icon height={28} width={28} />
     </div>
   );
 };
